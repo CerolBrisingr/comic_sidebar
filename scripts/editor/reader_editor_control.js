7f895c1c3b50a729dcf446b3421381abae40c361
Sebastian <bastianbaur@gmx.de>
2023-09-14 10:40:15 +0200

    Renaming things to make room

--- scripts/editor/reader_editor_control.js
+++ scripts/editor/reader_editor_control.js
@@ -29,7 +29,7 @@ class ReaderEditorControl {
         ReaderEditorControl.port.sendMessage("close"); // close any open windows
         let createData = {
             type: "detached_panel",
-            url: "../../editor/editor.html",
+            url: "../../editor/editor_panel.html",
             width: 500,
             height: 300
           };
