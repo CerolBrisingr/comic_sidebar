2b77e9533cb5a91ad853cc04792e13485fb7c2c6
Sebastian <bastianbaur@gmx.de>
2023-09-20 15:07:27 +0200

    Can edit top layer
    ! Changes in top layer are handed out but not available on next edit

--- scripts/editor/schedule_editor.js
+++ scripts/editor/schedule_editor.js
@@ -1,31 +1,71 @@
 class ScheduleEditor {
-    #ruleRadios;
+    #always;
+    #duration;
+    #weekly;
+    #monthly;
+    #schedule;
 
-    constructor(readerSchedule, fcnUpdate) {
-        this.#ruleRadios = document.getElementsByName("schedule");
-        this.#importSchedule(readerSchedule.returnAsObject(), fcnUpdate);
+    constructor(readerSchedule) {
+        this.#schedule = readerSchedule;
+        let myInterface = new EditorInterface(this);
+        this.#always = new BasicEditor("always", "always_frame", readerSchedule.getAlwaysOption(), myInterface);
+        this.#duration = new BasicEditor("duration", "duration_frame", readerSchedule.getDurationOption(), myInterface);
+        this.#weekly = new BasicEditor("weekly", "weekly_frame", readerSchedule.getWeeklyOption(), myInterface);
+        this.#monthly = new BasicEditor("monthly", "monthly_frame", readerSchedule.getMonthlyOption(), myInterface);
+        this.updateActivityChecks();
     }
 
-    #importSchedule(rule, fcnUpdate) {
-        for (const element of this.#ruleRadios) {
-            element.checked = (element.value === rule);
-            element.addEventListener("change", () => {
-                this.#updateSchedule(fcnUpdate);
-            });
+    updateActivityChecks() {
+        this.#always.updateState();
+        this.#duration.updateState();
+        this.#weekly.updateState();
+        this.#monthly.updateState();
+    }
+}
+
+class EditorInterface {
+    #scheduleEditor;
+
+    constructor(scheduleEditor) {
+        this.#scheduleEditor = scheduleEditor;
+    }
+
+    updateActivityChecks() {
+        this.#scheduleEditor.updateActivityChecks();
+    }
+
+}
+
+class BasicEditor {
+    constructor(checkbox, div, schedule, parentInterface) {
+        this._checkbox = document.getElementById(checkbox);
+        this._div = document.getElementById(div);
+        this._schedule = schedule;
+        this._interface = parentInterface;
+        this._checkbox.addEventListener("click", (evt) => {
+            this._schedule.setActive();
+            // Seems like you cannot listen to the state of the scheduler.
+            // Doing it this way then.
+            this._interface.updateActivityChecks();
+        });
+    }
+
+    updateState() {
+        if (this._schedule.isActive()) {
+            this._checkbox.checked = true;
+            this._expand();
+        } else {
+            this._checkbox.checked = false;
+            this._collapse();
         }
     }
 
-    #updateSchedule(fcnUpdate) {
-        fcnUpdate(this.#gatherSelectedSchedule());
+    _expand() {
+        this._div.classList.remove("no_draw");
     }
 
-    #gatherSelectedSchedule() {
-        for (const element of this.#ruleRadios) {
-            if (element.checked) {
-                return element.value;
-            }
-        }
-        return "none";
+    _collapse() {
+        this._div.classList.add("no_draw");
     }
 }
 
