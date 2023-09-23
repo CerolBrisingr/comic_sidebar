bd6e4b26563d157dc04043c2815546edbaa06243
Sebastian <bastianbaur@gmx.de>
2023-09-23 15:56:36 +0200

    bringing saves back

--- scripts/shared/web_reader.js
+++ scripts/shared/web_reader.js
@@ -193,7 +193,6 @@ class WebReaderBackground extends WebReader {
     }
     
     saveProgress() {
-        return;
         if (this._savingSuspended)
             return;
         let comicDataObject = buildWebReaderObject(this._readerStorage.getList());
