741b789e86d95ca29f4613b49efaf92bf42361b1
Sebastian <bastianbaur@gmx.de>
2023-09-23 13:44:50 +0200

    Scheduler works on durations option

--- scripts/shared/scheduler.js
+++ scripts/shared/scheduler.js
@@ -1,7 +1,8 @@
 import { getShowAll } from "./backup_import.js";
 import { saveShowAll } from "./backup_export.js";
 
-let milliSecondsInADay = 1000*60*60*24;
+let millisecondsInADay = 1000*60*60*24;
+let millisecondsInAnHour = 1000*60*60;
 let weekdayMap = new Map([
     ["sunday", 0],
     ["monday", 1],
@@ -34,6 +35,9 @@ class Scheduler {
             case "weekly":
                 this.#rule = this.#getWeeklyFcn(schedule);
                 break;
+            case "duration":
+                this.#rule = this.#getDurationFcn(schedule);
+                break;
             default:
                 this.#rule = (now, lastInteraction) => {
                     const compareTime = startOfDay(now);
@@ -52,6 +56,50 @@ class Scheduler {
         return this.#rule(now, lastInteraction);
     }
 
+    #getDurationFcn(schedule) {
+        const amount = schedule.getAmount();
+        switch (schedule.getUnit()) {
+            case "hours":
+                return this.#getHourDurationFcn(amount);
+            case "days":
+                return this.#getDayDurationFcn(amount);
+            case "weeks":
+                return this.#getDayDurationFcn(amount * 7);
+            case "months":
+                return this.#getMonthDurationFcn(amount);
+            default:
+                throw new Error("Unknown duration unit");
+        }
+    }
+
+    #getHourDurationFcn(amount) {
+        amount = Math.max(1, amount);
+        const distance = fromHours(amount);
+        return (now, lastInteraction) => {
+            const compareTime = now - distance;
+            return compare(compareTime, lastInteraction);
+        }
+    }
+
+    #getDayDurationFcn(amount) {
+        amount = Math.max(1, amount);
+        // startOfDay is already fulfilling 1 day distance
+        const distance = fromDays(amount -1);
+        return (now, lastInteraction) => {
+            const compareTime = startOfDay(now) - distance;
+            return compare(compareTime, lastInteraction);
+        }
+    }
+
+    #getMonthDurationFcn(amount) {
+        amount = Math.max(1, amount);
+        return (now, lastInteraction) => {
+            let compareTime = monthsAgo(now, amount) 
+            compareTime += fromDays(1); // Interact on 10th, allow again on 10th
+            return compare(compareTime, lastInteraction);
+        }
+    }
+
     #getWeeklyFcn(schedule) {
         let numDays = [];
         for (let day of schedule.getDays()) {
@@ -61,9 +109,16 @@ class Scheduler {
         }
         return (now, lastInteraction) => {
             const compareTime = startOfDay(now) - fromDays(minWeekDaySpan(now, numDays));
-            return compareTime > lastInteraction;
+            return compare(compareTime, lastInteraction);
         };
     }
+}
+
+function compare(compareTime, lastInteraction) {
+    // TODO: replace this by first set of unit tests
+    //console.log(`Difference in hours: ${(compareTime - lastInteraction)/1000/60/60}`);
+    //console.log(`Difference in days:  ${(compareTime - lastInteraction)/1000/60/60/24}`);
+    return compareTime > lastInteraction;
 }
 
 function startOfHour(now) {
@@ -90,16 +145,38 @@ function minWeekDaySpan(now, numDays) {
     return distance;
 }
 
+function monthsAgo(now, amount) {
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let day = now.getDate();
+    // apply duration
+    year -= Math.floor(amount / 12);
+    month -= amount % 12;
+    // Fix negative month
+    if (month < 1) {
+        year -= 1;
+        month += 12;
+    }
+    // Make sure that month has date, otherwise use 1st of next month
+    let daysInMonth = new Date(year, month, 0).getDate();
+    if (day > daysInMonth) {
+        month +=1;
+        day = 1;
+        // December has 31 days, will not jump years this way
+    }
+    return new Date(year, month, day).getTime();
+}
+
 function dayOfWeek(now) {
     return now.getDay(); // 0 = Sunday .. 6 = Saturday
 }
 
-function fromDays(timespan) {
-    return timespan * milliSecondsInADay;
+function fromHours(timespan) {
+    return timespan * millisecondsInAnHour;
 }
 
-function toSeconds(timespan) {
-    return timespan/1000;
+function fromDays(timespan) {
+    return timespan * millisecondsInADay;
 }
 
 class ShowAllInterface {
