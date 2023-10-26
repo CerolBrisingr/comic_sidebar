d3e784f2eb4b502e51526e83a466c31e2d0fa02e
Sebastian <bastianbaur@gmx.de>
2023-10-26 16:56:00 +0200

    Get rid of 2 redundant class properties

--- scripts/sidebar/canvas_icon.js
+++ scripts/sidebar/canvas_icon.js
@@ -1,15 +1,11 @@
 class CanvasIcon {
     #canvas;
     #context;
-    #width;
-    #height;
 
     constructor(canvas, source) {
         if (typeof canvas === "string")
             canvas = document.getElementById(canvas);
         this.#canvas = canvas;
-        this.#width = this.#canvas.width;
-        this.#height = this.#canvas.height;
         this.#context = this.#canvas.getContext("2d");
 
         if (source !== undefined)
@@ -21,7 +17,9 @@ class CanvasIcon {
         image.src = source;
         await image.decode();
 
-        this.#context.drawImage(image, 0, 0, this.#width, this.#height);
+        const width = this.#canvas.width;
+        const height = this.#canvas.height;
+        this.#context.drawImage(image, 0, 0, width, height);
     }
 }
 
