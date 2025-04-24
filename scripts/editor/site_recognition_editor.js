a65df3e2309c1c355ff89234ec1f94e0afd2bc0a
Sebastian <bastianbaur@gmx.de>
2025-04-25 00:15:09 +0200

    Editor looks ugly but works
    * CSS comes later, when aliases and titles are in place

--- scripts/editor/site_recognition_editor.js
+++ scripts/editor/site_recognition_editor.js
@@ -2,9 +2,9 @@ import { PrefixSelector } from "./prefix_selector.js";
 
 class SiteRecognitionEditor {
 
-    #parentDiv
-    #uiUpdateTrigger
-    #siteEditors = []
+    #parentDiv;
+    #uiUpdateTrigger;
+    #siteEditors = [];
 
     constructor(parentDiv, siteRecognition, uiUpdateTrigger) {
         this.#parentDiv = parentDiv;
@@ -14,6 +14,7 @@ class SiteRecognitionEditor {
 
     #buildInterface(siteRecognition) {
         for (let site of siteRecognition.getSites()) {
+            this.#setUpLabel(this.#parentDiv);
             this.#siteEditors.push(new SiteEditor(
                 this.#parentDiv,
                 site, 
@@ -21,42 +22,60 @@ class SiteRecognitionEditor {
         }
     }
 
+    #setUpLabel(parent) {
+        const label = HTML.insertElement(parent, "label");
+        HTML.addCssProperty(label, "div_label");
+        HTML.addText(label, "I recognize myself due to..");
+    }
+
 }
 
 class SiteEditor {
-    #site
-    #frame
-    #updateTrigger
+    #site;
+    #frame;
+    #prefixControl;
+    #updateTrigger;
+
+    #prefixEval;
 
     constructor(parent, site, updateTrigger) {
         this.#site = site;
         this.#updateTrigger = updateTrigger;
         this.#setUpFrame(parent);
-        this.#createIcon();
-        this.#setUpControls();
+
+        this.#introduceEvaluation(this.#frame);
+        this.#buildUrlEvaluation(this.#frame);
+
+        // Needs to happen late since it has to initially set the eval elements
+        this.#finalizeControls();
     }
 
     #setUpFrame(parent) {
         this.#frame = HTML.insertElement(parent, "div");
-        HTML.addCssProperty(this.#frame, "spans");
-        this.#frame.tabindex = "0";
+        HTML.addCssProperty(this.#frame, "config_container");
+
+        this.#prefixControl = HTML.insertElement(this.#frame, "div");
+        HTML.addCssProperty(this.#prefixControl, "spans");
+        this.#prefixControl.tabindex = "0";
+
+        const icon = this.#createIcon(this.#prefixControl);
+        icon.src = "../../icons/edit.svg";
     }
 
-    #createIcon() {
-        let icon = HTML.insertElement(this.#frame, "img");
+    #createIcon(parent) {
+        let icon = HTML.insertElement(parent, "img");
         HTML.addCssProperty(icon, "field_type_icon");
-        icon.src = "../../icons/edit.svg";
-        HTML.addSpacer(this.#frame);
+        HTML.addSpacer(parent);
         return icon;
     }
 
-    #setUpControls() {
-        let group = HTML.insertElement(this.#frame, "div");
+    #finalizeControls() {
+        let group = HTML.insertElement(this.#prefixControl, "div");
         let ui = {
             main: HTML.addSpan(group, "-main-", "prefix_main"),
             edge: HTML.addSpan(group, "-edge-", "prefix_edge"),
             trail: HTML.addSpan(group, "-tail-", "prefix_trail"),
-            prefixLine: this.#frame
+            prefixLine: this.#prefixControl
         }
         new PrefixSelector(
             this.#site.getLastUrl(), 
@@ -65,13 +84,32 @@ class SiteEditor {
             ui);
     }
 
+    #introduceEvaluation(parent) {
+        const span = HTML.insertElement(parent, "span");
+        const text = HTML.insertElement(span, "b");
+        text.innerText = "How does this match up?"
+    }
+
+    #buildUrlEvaluation(parent) {
+        const frame = HTML.insertElement(parent, "div");
+        HTML.addCssProperty(frame, "spans");
+
+        const icon = this.#createIcon(frame);
+        icon.src = "../../icons/information.svg";
+
+        const group = HTML.insertElement(frame, "div");
+        this.#prefixEval = HTML.addSpan(group, "-prefix-", "prefix_main");
+        HTML.addSpan(group, "****", "prefix_trail");
+    }
+
     #receiveNewPrefix(prefix) {
         this.#site.updatePrefix(prefix);
+        this.#prefixEval.innerText = prefix;
         this.#updateTrigger();
     }
 }
 
-// TODO: make own module and make use more widespread
+// TODO: make this it's own module
 class HTML {
     static insertElement(parent, strType) {
         let element = document.createElement(strType);
@@ -93,9 +131,13 @@ class HTML {
     }
 
     static addSpacer(parent) {
-        const space = document.createTextNode('\u00A0');
-        parent.appendChild(space);
-        return space;
+        return HTML.addText(parent, '\u00A0');
+    }
+
+    static addText(parent, strText) {
+        const text = document.createTextNode(strText);
+        parent.appendChild(text);
+        return text;
     }
 }
 
